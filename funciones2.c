27f54285f41c57e4710b21ab4fe9f6735734a3a0
johaneris <jsavalos@uamv.edu.ni>
2024-04-16 11:40:02 -0600

    ejercicio 2 funciones 2

---
+++ funciones2.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+/*Calculae la nota de un estudiante que posee un acumulado de 60 puntos y un examen de 40 puntos
+Decir i esta aprobado o reprobado. Un estudaidnte apeba su es mayor o igual a 70*/
+
+int calFinalSc(int score, int exam);
+
+int valFinalSc(int score);
+
+void pedirNotas();
+
+int main(int argc, char const *argv[])
+{
+    pedirNotas();
+    return 0;
+
+}
+
+void pedirNotas(){
+    int acumulado, examen, nota, notaFinal;
+    printf("Dime el acumulado: ");
+    scanf("%d", &acumulado);
+    printf("dime la nota del examen: ");
+    scanf("%d", &examen);
+    notaFinal = calFinalSc(acumulado, examen);
+    printf("nota final %d\n", notaFinal);
+}
+
+int calFinalSc(int score, int exam){
+    return score + exam;
+}
